d788f89c5046961e1fbfbae4b4ee7a02fe910c9f
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-07-17 16:01:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slides img');
 
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.remove('active');
-        if (i === index) {
-            slide.classList.add('active');
-        }
-    });
-}
-
 function nextSlide() {
+    slides[currentSlide].classList.remove('active');
     currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
+    slides[currentSlide].classList.add('active');
 }
 
-setInterval(nextSlide, 3000);
-document.addEventListener('DOMContentLoaded', () => {
-    showSlide(currentSlide);
-});
+setInterval(nextSlide, 5000); 
